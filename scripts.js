ba36a8c080212bdb6e1687f4a812773de6bc012b
Zigman <38165745+zhy61@users.noreply.github.com>
2019-10-15 10:50:02 -0700

    Test

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var message = "Hello from Treehouse!";
+alert(message);
+document.write("<h1> Welcome to JS World!</h1>");
+var visitorName = prompt("What is your name?");
+document.write("Hello, " + visitorName);
+console.log(visitorName);
+console.log("Program Complete!");
